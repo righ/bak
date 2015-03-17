6f73a883a97031d825cdb3c0369e7a67969dadce
crohaco <crohaco.net@gmail.com>
2015-03-18 02:11:40 +0900

    add behavior(remove)

---
+++ bak/behavior/remove.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+import os
+from .base import BaseBehavior
+
+
+class RemoveBehavior(BaseBehavior):
+    def __init__(self, *args):
+        """
+        """
+        assert 1 <= len(args) <= 3
+
+        if len(args) == 1:
+            i = args[0]
+            if i >= 0:
+                self.slice = slice(i, i+1)
+            else:
+                self.slice = slice(i-1, i)
+        else:
+            self.slice = slice(*args)
+
+    def do(self, item):
+        for archivename in item.history[self.slice]:
+            archivepath = os.path.join(item.savepath, backup_file)
+            os.remove(archivepath)
+

dfe8b362e0f4bf751fe4f94dfeb322441f2abde7
crohaco <crohaco.net@gmail.com>
2015-03-19 23:54:33 +0900

    renamed variables

--- bak/item.py
+++ bak/item.py
@@ -15,35 +15,35 @@ from .exception import (
     InvalidDatetimeUnit,
 )
 
-BASE_DATETIME_FORMAT = '%Y%m%d%H%M%S%f'
+BASE_TIMEFORMAT = '%Y%m%d%H%M%S%f'
 
 
 class Item(object):
     def __repr__(self):
-        return '<{0} ({1})>'.format(self.basename, len(self.history))
+        return '<{0} ({1})>'.format(self._basename, len(self.history))
 
     def __init__(
         self,
         path,
         savepath=None,
         archiver=TBZArchiver,
-        datetime_unit='second',
+        timeunit='second',
         **options
     ):
-        self.path = path.rstrip('/')
-        self.basename = os.path.basename(self.path)
+        self._path = path.rstrip('/')
+        self._basename = os.path.basename(self._path)
 
         try:
             index = {
                 'year': 2, 'month': 4, 'day': 6,
                 'hour': 8, 'minute': 10, 'second': 12, 'microsecond': 14,
-            }[datetime_unit.lower()]
-            self.datetime_format = BASE_DATETIME_FORMAT[:index]
+            }[timeunit.lower()]
+            self._timeformat = BASE_TIMEFORMAT[:index]
         except KeyError:
             raise InvalidDatetimeUnit
 
-        self.startswith = re.compile('^{basename}\.{suffix}'.format(
-            basename=re.escape(self.basename),
+        self._startswith = re.compile('^{basename}\.{suffix}'.format(
+            basename=re.escape(self._basename),
             suffix=r'(\d{4,21})'
         ))
 
@@ -52,44 +52,44 @@ class Item(object):
             raise ItemNotFound(abspath)
 
         self.options = options
-        self.archiver = archiver(**options)
+        self._archiver = archiver(**options)
 
-        self.isdir = os.path.isdir(abspath)
-        if self.isdir:
-            self.remove = shutil.rmtree
+        self._isdir = os.path.isdir(abspath)
+        if self._isdir:
+            self._remove = shutil.rmtree
         else:
-            self.remove = os.remove
+            self._remove = os.remove
 
-        for self.basename in reversed(abspath.split('/')):
-            if self.basename:
+        for self._basename in reversed(abspath.split('/')):
+            if self._basename:
                 break
         else:
             raise ItemPathInvalid(abspath)
 
         # if no problem, make save dir
-        self.savepath = savepath or os.path.dirname(self.path) or './'
-        if not os.path.exists(self.savepath):
-            os.makedirs(self.savepath)
+        self._savepath = savepath or os.path.dirname(self._path) or './'
+        if not os.path.exists(self._savepath):
+            os.makedirs(self._savepath)
 
         self.handlers = []
 
     @property
     def history(self):
         return sorted([
-            item for item in os.listdir(self.savepath)
-            if self.startswith.search(item)
+            item for item in os.listdir(self._savepath)
+            if self._startswith.search(item)
         ], reverse=True)
 
-    def temporary(self):
-        suffix = '.' + datetime.now().strftime(self.datetime_format)
-        tmppath = os.path.join(self.savepath, self.basename + suffix)
-        tmppath = self.archiver.compress(self.path, tmppath, isdir=self.isdir)
+    def _temporary(self):
+        suffix = '.' + datetime.now().strftime(self._timeformat)
+        tmppath = os.path.join(self._savepath, self._basename + suffix)
+        tmppath = self._archiver.compress(self._path, tmppath, isdir=self._isdir)
         history = self.history
 
         try:
             nochange = filecmp.cmp(
-                os.path.join(self.savepath, history[0]),
-                os.path.join(self.savepath, history[1])
+                os.path.join(self._savepath, history[0]),
+                os.path.join(self._savepath, history[1])
             )
         except IndexError:
             nochange = False
@@ -97,7 +97,7 @@ class Item(object):
         return nochange, tmppath
 
     def save(self, force=False):
-        nochange, newpath = self.temporary()
+        nochange, newpath = self._temporary()
         if not force and nochange:
             os.remove(newpath)
         self.trigger(**self.options)
@@ -105,15 +105,15 @@ class Item(object):
     def restore(self, force=False, refuge_current=True):
         history = self.history
         if not history:
-            raise HistoryEmpty(os.listdir(self.savepath))
+            raise HistoryEmpty(os.listdir(self._savepath))
 
-        nochange, curpath = self.temporary()
+        nochange, curpath = self._temporary()
         latestpath = history[0]
 
         if force or not nochange:
-            self.remove(self.path)
-            archive = os.path.join(self.savepath, latestpath)
-            self.archiver.decompress(archive, self.path, isdir=self.isdir)
+            self._remove(self._path)
+            archive = os.path.join(self._savepath, latestpath)
+            self._archiver.decompress(archive, self._path, isdir=self._isdir)
             os.remove(archive)
 
         if refuge_current:
@@ -124,8 +124,27 @@ class Item(object):
         else:
             os.remove(curpath)
 
-    def on(self, behavior, *conditions):
-        self.handlers.append(Handler(behavior, conditions))
+    def remove(self, *indexes):
+        assert 1 <= len(indexes) <= 3
+
+        if len(indexes) == 1:
+            i = indexes[0]
+            if i >= 0:
+                indexes = slice(i, i+1)
+            elif i == -1:
+                indexes = slice(-1, None)
+            else:
+                indexes = slice(i-1, i)
+        else:
+            indexes = slice(*indexes)
+
+        for archivename in self.history[indexes]:
+            archivepath = os.path.join(self._savepath, archivename)
+            os.remove(archivepath)
+
+    def on(self, behavior, signal):
+        handler = Handler(behavior, signal)
+        self.handlers.append(handler)
         return self
 
     def trigger(self, **env):

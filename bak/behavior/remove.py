61d9890ecd76235818e7a94f05a19f11b671bd82
crohaco <crohaco.net@gmail.com>
2015-03-19 23:55:04 +0900

    use item.remove

--- bak/behavior/remove.py
+++ bak/behavior/remove.py
@@ -1,24 +1,15 @@
 # coding: utf-8
-import os
+
 from .base import BaseBehavior
 
 
 class RemoveBehavior(BaseBehavior):
-    def __init__(self, *args):
+    def __init__(self, *indexes):
         """
         """
-        assert 1 <= len(args) <= 3
+        assert 1 <= len(indexes) <= 3
 
-        if len(args) == 1:
-            i = args[0]
-            if i >= 0:
-                self.slice = slice(i, i+1)
-            else:
-                self.slice = slice(i-1, i)
-        else:
-            self.slice = slice(*args)
+        self.indexes = indexes
 
     def execute(self, item, **env):
-        for archivename in item.history[self.slice]:
-            archivepath = os.path.join(item.savepath, archivename)
-            os.remove(archivepath)
+        item.remove(*self.indexes)

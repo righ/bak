fb7b07ea170e4dab91d956779e209294814f1b37
crohaco <crohaco.net@gmail.com>
2015-03-21 18:18:26 +0900

    fixed delta type evalute

--- bak/signal/lasttime.py
+++ bak/signal/lasttime.py
@@ -1,9 +1,12 @@
 # coding: utf-8
-from datetime import datetime
+from datetime import datetime, timedelta
+typedelta = (timedelta,)
+
 try:
-    from dateutil.relativedelta import relativedelta as timedelta
+    from dateutil.relativedelta import relativedelta
+    typedelta += (relativedelta,)
 except ImportError:
-    from datetime import timedelta
+    pass
 
 from .base import BaseSignal
 from ..exception import BakException
@@ -14,21 +17,23 @@ class InvalidDeltaType(BakException):
 
 
 class LastTimeSignal(BaseSignal):
-    def __init__(self, delta):
+    def __init__(self, delta, missing=True):
         if isinstance(delta, dict):
             self.delta = timedelta(**delta)
         elif isinstance(delta, (list, tuple)):
             self.delta = timedelta(*delta)
-        elif isinstance(delta, timedelta):
+        elif isinstance(delta, typedelta):
             self.delta = delta
         else:
             raise InvalidDeltaType(delta)
+        self.missing = missing
 
     def evalute(self, item, **env):
         try:
-            lasttime = datetime.strptime(item.history[0], item.datetime_format)
+            baktime = item.history[0][len(item._basename)+1:]
+            lasttime = datetime.strptime(baktime.split('.')[0], item._timeformat)
         except IndexError:
-            return False
+            return self.missing
 
         now = env.get('now', datetime.now())
-        return lasttime + self.delta > now
+        return lasttime + self.delta >= now
